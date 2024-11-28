f8408b30c2d95ad22b72b989db387f9b8c8d100c
Neitt_UwU <nathancarraranogueira@gmail.com>
2024-11-27 22:09:11 -0300

    Update and rename Index.php to index.php

--- index.php
+++ index.php
@@ -7,11 +7,8 @@ require_once('classes/Magia.php');
 require_once('classes/Inventario.php');
 require_once('classes/Player.php');
 
-$inventario1 = new Inventario(20);
-$inventario2 = new Inventario(20);
-
-$player1 = new Player("Steve", 1, $inventario1);
-$player2 = new Player("Alex", 1, $inventario2);
+$player1 = new Player("Steve");
+$player2 = new Player("Alex");
 
 $ataque1 = new Ataque("Espada de Diamante");
 $ataque2 = new Ataque("Espada de Netherita");
@@ -24,9 +21,6 @@ $defesa3 = new Defesa("Elmo de Ouro");
 $magia1 = new Magia("Varinha de Blaze");
 $magia2 = new Magia("Pó de Redstone");
 $magia3 = new Magia("Totem da Imortalidade");
-
-echo "A capacidade livre no inventário é de " . $inventario1->capacidadeLivre() . " espaços. <br>";
-echo "A capacidade livre no inventário é de " . $inventario2->capacidadeLivre() . " espaços. <br><br>";
 
 $player1->coletarItem($ataque1);
 $player1->coletarItem($ataque2);
